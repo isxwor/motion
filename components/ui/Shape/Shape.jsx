isxwor/motion

commit 284cfb70695ef3e6ffc05c90489a5a22728e9e4c

add `Shape` component

components/ui/Shape/Shape.jsx
@@ -1,0 +1,15 @@
+import { styled } from 'styletron-react';
+
+const mapShapeToBorderStyle = {
+  square: '1em',
+  cirlce: '50%',
+};
+
+const Shape = styled('div', ({ $shape }) => ({
+  height: '5em',
+  width: '5em',
+  background: 'var(--color-primary)',
+  borderRadius: mapShapeToBorderStyle[$shape] || mapShapeToBorderStyle.square,
+}));
+
+export default Shape;

components/ui/Shape/index.js
@@ -1,0 +1,1 @@
+export { default } from './Shape';

components/ui/index.js
@@ -1,2 +1,3 @@
 export { default as Card } from './Card';
 export { default as Container } from './Container';
+export { default as Shape } from './Shape';

pages/index.jsx
@@ -1,12 +1,19 @@
 import { styled } from 'styletron-react';
 
 import { Layout } from '@components/common';
-import { Card, Container } from '@components/ui';
+import { Card, Container, Shape } from '@components/ui';
 
 const Home = () => (
   // eslint-disable-next-line no-use-before-define
   <Container $as={Grid}>
-    <Card>Playgorund</Card>
+    <Card
+      $sx={{
+        position: 'relative',
+        overflow: 'hidden',
+      }}
+    >
+      <Shape />
+    </Card>
     <Card>Controls</Card>
   </Container>
 );

commit 6d304e2edaeeb92c34137583827b88c7f03cc233

fix typo `cirlce` -> `circle` introduced in 284cfb7

components/ui/Shape/Shape.jsx
@@ -2,7 +2,7 @@ import { styled } from 'styletron-react';
 
 const mapShapeToBorderStyle = {
   square: '1em',
-  cirlce: '50%',
+  circle: '50%',
 };
 
 const Shape = styled('div', ({ $shape }) => ({