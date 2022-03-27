2110592786fa966b4efa29f1cc9cde0eb8fe221d
greyhere <57859708+greyhere@users.noreply.github.com>
2022-03-28 00:08:40 +0530

    fix typo `square` -> `SQUARE`

--- components/ui/Shape/Shape.jsx
+++ components/ui/Shape/Shape.jsx
@@ -9,7 +9,7 @@ const Shape = styled('div', ({ $shape, $sx }) => ({
   height: '5em',
   width: '5em',
   background: 'var(--color-primary)',
-  borderRadius: mapShapeToBorderStyle[$shape] || mapShapeToBorderStyle.square,
+  borderRadius: mapShapeToBorderStyle[$shape] || mapShapeToBorderStyle.SQUARE,
   // this will be dynamically generated in future
   transition: '200ms border-radius ease',
   ...$sx,
