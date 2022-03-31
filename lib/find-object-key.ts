0dcefb890d3f73be0c7bb2af4234b6706dd360c7
greyhere <57859708+greyhere@users.noreply.github.com>
2022-03-31 12:45:16 +0530

    remove `find-object-key.ts` helper function

--- lib/find-object-key.ts
+++
@@ -1,7 +0,0 @@
-const findObjectKey = (
-  object: Record<string, string | number>,
-  value: string | number
-): string | undefined =>
-  Object.keys(object).find((key) => object[key] === value);
-
-export default findObjectKey;
